9b7112799e13d2ae83d8b7ab05daabbd45b3959b
WolfieLux <61132724+WolfieLux@users.noreply.github.com>
2023-05-28 09:10:16 +0800

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export default {
+  //Site display name
+  SITE_TITLE: "WolfiesCinema - Freemium is our Goal",
+
+  //Put your discord link here or null to disable
+  SITE_DISCORD: "https://discord.gg/area51ware",
+  
+  //True or false to show or hide site credit in footer (You should leave enabled 😉)
+  SITE_CREDITS: false,
+
+  //Do not change or site will break!
+  RIPPER_API: "http://139.99.155.204:3000"
+}
